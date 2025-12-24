2c646316aa44ce4536be771b4ae8338269447137
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2025-12-24 19:13:03 +0530

    this third commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 5000);
+    const timer = setTimeout(() => setLoading(false), 0);
     return () => clearTimeout(timer);
   }, []);
 
